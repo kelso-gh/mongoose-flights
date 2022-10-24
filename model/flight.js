981e9bc0ca36fcbe159312f30e07c4d4171bc6bf
kelso-gh <soyouwontsingg@gmail.com>
2022-10-24 18:16:03 -0400

    Flight schema

--- model/flight.js
+++ model/flight.js
@@ -1,1 +1,25 @@
 const mongoose = require('mongoose');
+
+
+const flightSchema = new mongoose.Schema({
+    airport: {
+        type: String,
+        enum: ['AUS', 'DFW', 'DEN', 'LAX', 'JFK'],
+        default: 'LAX'
+    },
+    airline: {
+        type: String,
+        enum: ['American', 'Alaska', 'JetBlue']
+    },
+    flightNo: {
+        type: Number,
+        min: 10,
+        max: 9999
+    },
+    departs: {
+        type: Date,
+        default: function() {
+            return new Date().getDate();
+        } //How do you implement one year from date created?
+    }
+});
